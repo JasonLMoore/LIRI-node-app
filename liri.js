7e8dee7802838ba5358af9d7ac19ef5d74160952
Jason Moore <jason.moore1369@gmail.com>
2019-06-17 17:00:22 -0500

    concertThis now has for-loop for bands with multi word names. pick function created and added (not sold on name?)

--- liri.js
+++ liri.js
@@ -21,17 +21,17 @@ var spotify = new Spotify(keys.spotify);
 //`node liri.js concert-this <artist/band name here>`//
 
 var concertThis = function() {
-  //var nodeArgs = process.argv
-  var artist = "ZZ Top";
+  var nodeArgs = process.argv
+  var artist = "";
 
-  //for (var i = 2; i < nodeArgs.length; i++) {
-    //if (i > 2 && i < nodeArgs.length) {
-      //artist = artist + "+" + nodeArgs[i];
-    //}
-    //else {
-      //artist += nodeArgs[i];
-    //}
-  //}
+  for (var i = 2; i < nodeArgs.length; i++) {
+    if (i > 2 && i < nodeArgs.length) {
+      artist = artist + "+" + nodeArgs[i];
+    }
+    else {
+      artist += nodeArgs[i];
+    }
+  }
 
   var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
   console.log("Query URL is " + queryURL);
@@ -48,7 +48,6 @@ var concertThis = function() {
       );
     }
   )
-
 };
 console.log(concertThis());
 /////////////////////////////////////////////////////////////////
@@ -144,3 +143,24 @@ var doTxt = function() {
   });
 };
 console.log(doTxt());
+/////////////////////////////////////////////////////////////
+
+//determins which command is used//
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+  case "concert-this":
+    concertThis(functionData);
+    break;
+  case "spotify-this-song":
+    spotifySearch(functionData);
+    break;
+  case "movie-this":
+    movieThis(functionData);
+    break;
+  case "do-what-it-says":
+    doTxt();
+    break;
+  default:
+    console.log("I'm affraid I can't do that friend.");
+  }
+};
